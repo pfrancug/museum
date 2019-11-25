013aa25f4690c2800845d7f17b68e1543e625205
Piotr Francug <piotr.francug@gmail.com>
2019-11-25 20:19:01 +0100

    add login panel

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 class LoginPage extends React.Component {
     constructor(props) {
         super(props)
     }
+
+    // routeChange() {
+    //     const path 
+    // }
 
     render() {
         return (
@@ -11,12 +16,12 @@ class LoginPage extends React.Component {
                 <h1 className="login__header">YOUR<br />ART<br />MUSEUM</h1>
                 <p className="login__address">151rd St<br />San Francosco, CA 94103</p>
                 <div className="login__credentials">
-                    <input className="login__email" type="text" placeholder="Email address" />
+                    <input className="login__email" type="email" placeholder="Email address" />
                     <input className="login__password" type="password" placeholder="Password" />
                 </div>
-                <a className="login__reset-password">Forgot your password?</a>
-                <button className="login__button">Log In</button>
-                <a className="login__create-account">Don't have an account?</a>
+                <NavLink className="login__reset-password" to='/account-recovery'>Forgot your password?</NavLink>
+                <NavLink className="login__button" to='/'>Log In</NavLink>
+                <NavLink className="login__create-account" to='/create-account'>Don't have an account?</NavLink>
             </div>
         )
     }
