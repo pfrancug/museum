ea045e40180287a41703a73a49f31f0b4283e3d3
Piotr Francug <piotr.francug@gmail.com>
2019-11-26 20:24:07 +0100

    feat - filter

--- src/components/Collections.js
+++ src/components/Collections.js
@@ -6,21 +6,37 @@ import collectionsData from './collections-data'
 class Collections extends React.Component {
     constructor(props) {
         super(props)
+        this.state = {
+            filter: ''
+        }
+        this.handleSetFilter = this.handleSetFilter.bind(this)
+    }
+
+    handleSetFilter(e) {
+        const filter = e.target.value
+        console.log(filter)
+        this.setState({ filter })
     }
 
     render() {
+        const filterdCollections = collectionsData.filter(collection => collection.title.toLocaleLowerCase().includes(this.state.filter.toLocaleLowerCase()))
         return (
             <div className="collections" >
                 <div className="collections__search">
-                    <input className="collections__search-input" type="text" placeholder="Expore the Collection" />
+                    <input
+                        className="collections__search-input"
+                        type="text"
+                        placeholder="Expore the Collection"
+                        onChange={this.handleSetFilter}
+                    />
                     <div className="collections__search-icon">
                         <FontAwesomeIcon icon={faSearch} />
                     </div>
                 </div>
                 <p className="collections__advanced-search">Advanced Search</p>
                 <div className="collections__elements">
-                    {collectionsData.length ? collectionsData.map((item) => (
-                        <div className="collections__element" >
+                    {filterdCollections.length ? filterdCollections.map((item, index) => (
+                        <div className="collections__element" key={index}>
                             <img className="collections__image" src={item.image} />
                             <p className="collections__text">{item.title}</p>
                         </div>
@@ -32,7 +48,6 @@ class Collections extends React.Component {
             </div>
         )
     }
-
 }
 
 export default Collections
