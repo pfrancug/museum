f87c454d57085e09eea18bef5172edd56f392abf
Piotr Francug <piotr.francug@gmail.com>
2019-11-26 15:07:18 +0100

    cleaning

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import AppRouter from './routes/AppRouter'
 import 'normalize.css'
-import '@fortawesome/fontawesome-free/js/all'
 import './styles/styles.scss'
 
 ReactDOM.render(<AppRouter />, document.getElementById('app'));
