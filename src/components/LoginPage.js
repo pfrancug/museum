2f73e98545580943d6a2804908e28e48130d7d0f
Piotr Francug <piotr.francug@gmail.com>
2019-11-26 14:36:09 +0100

    add - img as backgroud for Login Page

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -1,24 +1,26 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import backgroundImage from './../images/woman.jpg'
+import backgroundImage from './../images/login-background.jpg'
 
 class LoginPage extends React.Component {
     constructor(props) {
         super(props)
     }
-
     render() {
         return (
-            <div className="login" style={{ backgroundImage: `url(${backgroundImage})`, backgroundRepeat: 'no-repat', backgroundPosition: 'center' }}>
-                <h1 className="login__header">YOUR<br />ART<br />MUSEUM</h1>
-                <p className="login__address">151rd St<br />San Francosco, CA 94103</p>
-                <div className="login__credentials">
-                    <input className="login__email" type="email" placeholder="Email address" />
-                    <input className="login__password" type="password" placeholder="Password" />
+            <div className="login" >
+                <div className="login__bg-image" style={{ backgroundImage: `url(${backgroundImage})` }} />
+                <div className="login__panel">
+                    <h1 className="login__header">YOUR<br />ART<br />MUSEUM</h1>
+                    <p className="login__address">151rd St<br />San Francosco, CA 94103</p>
+                    <div className="login__credentials">
+                        <input className="login__email" type="email" placeholder="Email address" />
+                        <input className="login__password" type="password" placeholder="Password" />
+                    </div>
+                    <Link className="login__reset-password" to='/account-recovery'>Forgot your password?</Link>
+                    <Link className="login__button" to='/'>Log In</Link>
+                    <Link className="login__create-account" to='/create-account'>Don't have an account?</Link>
                 </div>
-                <Link className="login__reset-password" to='/account-recovery'>Forgot your password?</Link>
-                <Link className="login__button" to='/'>Log In</Link>
-                <Link className="login__create-account" to='/create-account'>Don't have an account?</Link>
             </div>
         )
     }
