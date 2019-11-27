c0ed6d30eaf92db78c53dab53371ce1333c2a0bf
Piotr Francug <piotr.francug@gmail.com>
2019-11-27 09:47:09 +0100

    perf. - small size images

--- src/components/collections-data.js
+++ src/components/collections-data.js
@@ -1,50 +1,26 @@
-// import decorativeArts from '../images/collections/decorative-arts.jpg'
-// import americanImpressionism from '../images/collections/american-impressionism.jpg'
-// import deStijl from '../images/collections/de-stijl.jpg'
-// import cubism from '../images/collections/cubism.jpg'
-// import american from '../images/collections/american.jpg'
-// import greekAntiquities from '../images/collections/greek-antiquities.jpg'
-
-// const collectionsData = [
-//     {
-//         title: 'DECORATIVE ARTS & CRAFTS',
-//         image: decorativeArts
-//     }, {
-//         title: 'AMERICAN IMPRESSIONISM',
-//         image: americanImpressionism
-//     }, {
-//         title: 'DE STIJL',
-//         image: deStijl
-//     }, {
-//         title: 'CUBISM',
-//         image: cubism
-//     }, {
-//         title: 'AMERICAN PHOTOGRAPHY',
-//         image: american
-//     }, {
-//         title: 'GREEK ANTIQUITIES',
-//         image: greekAntiquities
-//     }
-// ]
-
+import decorativeArts from '../images/collections/decorative-arts.jpg'
+import americanImpressionism from '../images/collections/american-impressionism.jpg'
+import deStijl from '../images/collections/de-stijl.jpg'
+import cubism from '../images/collections/cubism.jpg'
+import american from '../images/collections/american.jpg'
 import greekAntiquities from '../images/collections/greek-antiquities.jpg'
 
 const collectionsData = [
     {
         title: 'DECORATIVE ARTS & CRAFTS',
-        image: greekAntiquities
+        image: decorativeArts
     }, {
         title: 'AMERICAN IMPRESSIONISM',
-        image: greekAntiquities
+        image: americanImpressionism
     }, {
         title: 'DE STIJL',
-        image: greekAntiquities
+        image: deStijl
     }, {
         title: 'CUBISM',
-        image: greekAntiquities
+        image: cubism
     }, {
         title: 'AMERICAN PHOTOGRAPHY',
-        image: greekAntiquities
+        image: american
     }, {
         title: 'GREEK ANTIQUITIES',
         image: greekAntiquities
