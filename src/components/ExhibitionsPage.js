26171894b85b79d358dc59421b8b0aa10b711746
Piotr Francug <piotr.francug@gmail.com>
2019-11-27 13:47:59 +0100

    draft

--- src/components/ExhibitionsPage.js
+++ src/components/ExhibitionsPage.js
@@ -1,4 +1,7 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faMapMarkerAlt, faClock } from '@fortawesome/pro-duotone-svg-icons'
+import image from '../images/login-background.jpg'
 
 class ExhibitionsPage extends React.Component {
     constructor(props) {
@@ -7,7 +10,32 @@ class ExhibitionsPage extends React.Component {
 
     render() {
         return (
-            <h1>Exhibitions</h1>
+            <div className="exhibitions">
+
+                <img src={image}></img>
+
+                <p>some text</p>
+                <h1>some header</h1>
+                <h2>some details</h2>
+                <p>some other text</p>
+                <button>plan a visit</button>
+                <div>
+
+                    <div>
+                        <FontAwesomeIcon icon={faMapMarkerAlt}/>
+                        <p>some text</p>
+                        <p>some details</p>
+                    </div>
+                    <div>
+                    <FontAwesomeIcon icon={faClock}/>
+                    <p>some text</p>
+                        <p>some details</p>
+                    </div>
+
+                </div>
+
+
+            </div>
         )
     }
 
